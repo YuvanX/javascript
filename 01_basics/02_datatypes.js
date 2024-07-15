2bcde65984f575dea061453051dd2fbb148896fb
K. Abhi Vignesh <132771557+YuvanX@users.noreply.github.com>
2024-07-15 08:00:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+let myName = "Abhi"
+let age = 19
+let isLoggedIn = true
+
+// let state; undefined
+ 
+// Primitive datatypes
+
+// number
+// string
+// boolean
+// null -> standalone value
+// undefined
+// bigint
+// symbol -> unqiueness
+
+// Object
+// array
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
