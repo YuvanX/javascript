225fc33e294919c172adab8f3662833d1d3747e3
K. Abhi Vignesh <132771557+YuvanX@users.noreply.github.com>
2024-07-15 03:39:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountName = "abhi"
+let accountId = 12233
+
+// var accountPassword = "secret"     it does not have scope 
+
+// userState = "hyd" never declare variables like this
+
+
+let userStatus; //if not intialised then undefined 
+
+// accountName = "yuvan" not allowed
+
+
+
+console.table([accountName, accountId, accountPassword, userState, userStatus])
