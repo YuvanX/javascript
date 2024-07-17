18f6dd2dd4359e2f4230dcb15d4d6354efb30103
K. Abhi Vignesh <132771557+YuvanX@users.noreply.github.com>
2024-07-17 03:30:28 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+console.log("02" > 1) //true
+console.log("2" > 1) //true
+
+console.log(null > 0) //false
+console.log(null == 0) // false
+console.log(null >= 0) // true null converts to 0
+
+
+//===   => this even checks the datatype 
+console.log("2" === 2) // false
